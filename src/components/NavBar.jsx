ea974603538a5252c64611081eb611af38e2a7b7
joacotournier <87491798+joacotournier@users.noreply.github.com>
2022-02-22 22:06:46 -0300

    Prueba Git

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
