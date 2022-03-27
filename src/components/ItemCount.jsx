ef44123aa6a1706be9a46850e41b733f39a3ae5f
joacotournier <87491798+joacotournier@users.noreply.github.com>
2022-03-27 17:11:11 -0300

    Fix desafío

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 export const ItemCount = (props) => {
   const [count, setCount] = useState(props.initial);
@@ -16,15 +17,13 @@ export const ItemCount = (props) => {
     count > 0 && setStock(stock + 1);
   };
 
-  const goToCheckout = () => {
-    window.location.href = "/cart"
-    onAdd(count)
+  const ConditionalLink = () => { return (count == 0) ? <button>Treat Yourself</button> : <Link to={"/cart"} className="button" onClick={onAdd(count)} >Treat Yourself</Link>
   }
 
   return (
     <div>
       <img src="../less-circle-fill.svg" onClick={substract}></img>
-      <button onClick={goToCheckout} >Treat Yourself</button>
+      <ConditionalLink />
       <img src="../add-circle-fill.svg" onClick={add}></img>
       <p>Count: {count}</p>
       <p>Stock: {stock}</p>
